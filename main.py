761fc34ee6f5d1bdd066a51035b850d56e6fb63e
Pranav-Official <pranavhtjp07@gmail.com>
2023-04-17 11:23:36 +0530

    testing

--- main.py
+++ main.py
@@ -59,6 +59,7 @@ def upload():
             t.join()
             filenames.append(filename)
     # Call example function with uploads directory path
+    
     images_to_pdf(os.path.abspath(app.config['UPLOAD_FOLDER']))
     
     return render_template('download_pdf.html')
